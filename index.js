7421eea3cc1938d466e7fa29cad406c2d9ed7e41
michaljones <micheal.jones2010@yahoo.com>
2023-11-05 15:06:19 -0500

    done

---
+++ index.js
@@ -0,0 +1,16 @@
+
+let divide= function() {
+    return 2000/100;
+};
+
+const square=(number) =>{
+return number * number;
+
+}
+
+const add=(number1, number2) =>{
+   return  number1 + number2;
+
+
+
+}
